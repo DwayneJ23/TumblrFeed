4c6e595d83d8fe5e8faeff8489611f6c52df9133
Dwayne Johnson <dwaynejohnson@mac.home>
2017-02-02 01:35:40 -0500

    first commit

---
+++ TumblrFeed/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Dwayne Johnson on 2/2/17.
+//  Copyright © 2017 Dwayne Johnson. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
